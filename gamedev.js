b33d1f4dfe35aa8b73a6a7da68f5d5be4dd0fd1c
210727 <210727@hkis.edu.hk>
2016-05-19 09:39:29 +0800

    testing stuff
    to experiment how to change text in div upon “send” button

--- gamedev.js
+++ gamedev.js
@@ -1,5 +1,15 @@
 $( document ).ready(function() {
 
 //Your jquery + js goes here
+$(function() {
+  var copyText= [
+   'this is the first line',
+   'Simply put, the second line',
+   'impressed by the third line.'
+  ];
+  $("#thisbutton").click(function () {
+     $("#thetext").text(copyText[1]);
+  });
 
+});
 });
