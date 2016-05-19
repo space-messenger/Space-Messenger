aabb90b92ab245593a2366bbfcdab9eb6d0e49da
210727 <210727@hkis.edu.hk>
2016-05-19 09:44:34 +0800

    deleted change text testing

--- gamedev.js
+++ gamedev.js
@@ -1,15 +1,7 @@
 $( document ).ready(function() {
 
 //Your jquery + js goes here
-$(function() {
-  var copyText= [
-   'this is the first line',
-   'Simply put, the second line',
-   'impressed by the third line.'
-  ];
-  $("#submitmessage").click(function () {
-     $("#thetext").text(copyText[1]);
-  });
+
 
 });
 });
